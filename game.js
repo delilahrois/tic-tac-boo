0332ca0886ad897cde6c5315c885ad315b3d2d62
Delilah Necrason <delilahnecrason@gmail.com>
2021-09-25 18:23:25 -0400

    Add winning combos to game class , add arrays to keep track of placed tokens

--- game.js
+++ game.js
@@ -4,7 +4,20 @@ class Game {
     this.player2 = new Player('2', '👻');
     this.player1Turn = true;
     this.player2Turn = false;
-    // winning combos 
+    this.playedSquaresP1 = [];
+    this.playedSquaresP2 = [];
+    this.player1Wins = 0;
+    this.player2Wins = 0;
+    this.winner = null;
+    this.winningCombos =
+      [1, 2, 3],
+      [1, 4, 7],
+      [1, 5, 9],
+      [2, 5, 8],
+      [3, 6, 9],
+      [3, 5, 7],
+      [4, 5, 6],
+      [7, 8, 9];
   }
 
   switchTurns() {
@@ -16,12 +29,12 @@ class Game {
       this.player2Turn = false;
     }
   }
-}
+};
 
 // Game class should include:
-  // 2 instances of Player class
-  // a way to keep track of the data for the game board
-  // a way to keep track of which player's turn it is
+  // 2 instances of Player class $
+  // a way to keep track of the data for the game board $
+  // a way to keep track of which player's turn it is $
   // a way to check the Game's board data for win conditions
   // a way to detect when a game is a draw (no win)
   // a way to reset the Game's board to begin a new Game
