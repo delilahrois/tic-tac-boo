01657f87057e7c8241c684c9ed117840e4c020f3
Delilah Necrason <delilahnecrason@gmail.com>
2021-09-26 18:22:12 -0400

    Add minor property tweaks to game class

--- game.js
+++ game.js
@@ -8,7 +8,9 @@ class Game {
     this.playedSquaresP2 = [];
     this.player1Wins = 0;
     this.player2Wins = 0;
+    this.playedSquares = [];
     this.winner = null;
+    this.isOver = false;
     this.winningCombos = [
       ['1', '2', '3'],
       ['1', '4', '7'],
@@ -45,10 +47,17 @@ class Game {
     }
   }
 
-  // detectDraw() {
-  //   if ()
-  //   // if all squares have been played and there is no winner, this.winner = null and return string `It's a tie! Play again?`
-  // }
+  detectDraw() {
+    for (var i = 0; i < this.playedSquares.length; i++) {
+      if (this.playedSquares === ['1', '2', '3', '4', '5', '6', '7',
+        '8', '9']) {
+          this.isOver = true;
+        }}
+    if (!this.winner && this.isOver) {
+      header.innerText = `It's a tie! Play again?`;
+    }
+    // if all squares have been played and there is no winner, this.winner = null and return string `It's a tie! Play again?`
+  }
 };
 
 // Game class should include:
@@ -57,4 +66,4 @@ class Game {
   // a way to keep track of which player's turn it is $
   // a way to check the Game's board data for win conditions $
   // a way to detect when a game is a draw (no win)
-  // a way to reset the Game's board to begin a new Game
+  // a way to reset the Game's board to begin a new Game $ --- this is currently in the main.js file. refactor??
